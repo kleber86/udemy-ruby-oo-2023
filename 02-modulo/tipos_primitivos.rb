df78145f75748a85fe5e78fd0d62fcc0a73dac6c
Kleber Nascimento <klebernascimento@outlook.com.br>
2023-01-11 21:53:27 -0300

    Tipos primitivos e Variáveis

---
+++ 02-modulo/tipos_primitivos.rb
@@ -0,0 +1,22 @@
+# Tipos primitivos e variáveis
+
+puts 12.class # Integer
+puts 12.1.class # Float
+
+puts '1'.class # String
+puts 'Texto'.class # String
+
+puts true.class # TrueClass
+puts false.class # FalseClass
+
+
+# Variáveis
+valor1 = 10
+valor2 = 9
+
+puts valor1 + valor2
+
+nome = 'Kleber'
+idade = 36
+
+puts "Meu nome é #{nome} e tenho #{idade} anos."
