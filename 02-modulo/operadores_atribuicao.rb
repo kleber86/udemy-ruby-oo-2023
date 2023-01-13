99a86fd118a2773aa14efc8f054072e2cbd577e0
Kleber Nascimento <klebernascimento@outlook.com.br>
2023-01-13 20:05:11 -0300

    Operadores de Atribuição

---
+++ 02-modulo/operadores_atribuicao.rb
@@ -0,0 +1,10 @@
+nome = 'Kleber'
+puts nome # => Kleber
+
+valor = 1
+puts valor += 2  # => 3
+puts valor -= 2  # => 1
+puts valor *= 2  # => 2
+puts valor /= 2  # => 1
+puts valor %= 2  # => 1
+puts valor **= 2 # => 1
