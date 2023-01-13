b5920dadb728fac28ff59f4dd02efb1711e3c0ae
Kleber Nascimento <klebernascimento@outlook.com.br>
2023-01-12 21:18:09 -0300

    Operadores Relacionais

---
+++ 02-modulo/operadores_relacionais.rb
@@ -0,0 +1,7 @@
+puts "1 é maior que 2: #{1 > 2}"
+puts "2 é maior que 1: #{2 > 1}"
+puts "2 é igual a 2: #{2 == 2}"
+puts "2 é diferente de 3: #{2 != 3}"
+puts "Operador spaceship 1 <=> 2: #{1 <=> 2}"
+puts "Operador spaceship 2<=> 2: #{2 <=> 2}"
+puts "Operador spaceship 3 <=> 2: #{3 <=> 2}"
