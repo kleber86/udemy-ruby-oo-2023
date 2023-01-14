97ce4659a76900c3b4d4511a82b988cabb1491fe
Kleber Nascimento <klebernascimento@outlook.com.br>
2023-01-14 10:41:30 -0300

    Symbol

---
+++ 02-modulo/symbol.rb
@@ -0,0 +1,8 @@
+# Symbol
+
+puts :symbol_ruby.class # Symbol
+puts :symbol_ruby.object_id # 916828
+puts :symbol_ruby.object_id # 916828
+
+hash_symbol = {v1: "1", v2: "2"}
+puts hash_symbol # {:v1=>"1", :v2=>"2"}
