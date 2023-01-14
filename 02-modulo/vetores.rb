40cc0ddb02862203d1aeadeef93491803dab6d8f
Kleber Nascimento <klebernascimento@outlook.com.br>
2023-01-13 21:07:52 -0300

    Vetores / Arrays

---
+++ 02-modulo/vetores.rb
@@ -0,0 +1,8 @@
+# VETORES / ARRAYS
+numero = [1, 2, 3]
+puts "Valores do vetor: #{numero}"
+puts "Tamanho do verto: #{numero.length}"
+
+textos = ['A', 'B', 'C', 'D']
+puts "Valores do vetor: #{textos}"
+puts "Tamanho do vetor: #{textos.length}"
