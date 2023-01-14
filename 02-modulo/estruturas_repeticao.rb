a64bd582f6a5edfbd3e8f0861b10ebe5d95ad437
Kleber Nascimento <klebernascimento@outlook.com.br>
2023-01-13 21:01:41 -0300

    Estruturas de repetição

---
+++ 02-modulo/estruturas_repeticao.rb
@@ -0,0 +1,14 @@
+# WHILE / ENQUANTO FOR VERDADEIRO
+puts "---- WHILE ----"
+i = 0
+while i <= 50
+  puts "sou verdadeiro - #{i}"
+  i += 1
+end
+
+# UNTIL / ENQUANTO FOR FALSO
+puts "--- UNTIL ---"
+until i <= 0
+  puts "sou falso - #{i}"
+  i -= 1
+end
