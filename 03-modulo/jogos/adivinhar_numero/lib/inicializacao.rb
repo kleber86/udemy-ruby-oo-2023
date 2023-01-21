b46a9974ddf9996c6319d502e7e6031990b0c9ac
Kleber Nascimento <klebernascimento@outlook.com.br>
2023-01-20 22:08:43 -0300

    Executando comandos do SO

--- 03-modulo/jogos/adivinhar_numero/lib/inicializacao.rb
+++ 03-modulo/jogos/adivinhar_numero/lib/inicializacao.rb
@@ -1,10 +1,12 @@
 class Inicializacao
   def self.inicializando
+    system('clear')
     print "Inicializando."
     4.times do |i|
       sleep 1
       print "."
     end
     puts "."
+    system('clear')
   end
 end
