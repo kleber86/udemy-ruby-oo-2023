5de9a82bfcdbf3140ae332415e294d256c84765c
Kleber Nascimento <klebernascimento@outlook.com.br>
2023-01-21 22:17:13 -0300

    Gems para terminal

--- 03-modulo/jogos/adivinhar_numero/lib/inicializacao.rb
+++ 03-modulo/jogos/adivinhar_numero/lib/inicializacao.rb
@@ -1,18 +1,31 @@
+require 'tty-cursor'
+require 'tty-spinner'
+
 class Inicializacao
   def self.inicializando
     system('clear')
-    # print "Inicializando."
-    # 4.times do |i|
-    #   sleep 1
-    #   print "."
-    # end
-    # puts "."
-    File.open(File.expand_path('../../ascii/ascii.txt', __FILE__), 'r') do |arq|
-      while line = arq.gets
-        puts line
-      end
+
+    spinner = TTY::Spinner.new
+    spinner = TTY::Spinner.new("[:spinner] Loading ...", format: :pulse_2)
+    spinner.auto_spin # Automatic animation with default interval
+    sleep(5) # Perform task
+    spinner.stop("Done!") # Stop animation
+
+    system('clear')
+    cursor = TTY::Cursor
+    print cursor.move_to(60, 20)
+    print "Inicializando."
+    4.times do |i|
+      sleep 1
+      print "."
     end
-    sleep 6
+    puts "."
+    # File.open(File.expand_path('../../ascii/ascii.txt', __FILE__), 'r') do |arq|
+    #   while line = arq.gets
+    #     puts line
+    #   end
+    # end
+    # sleep 6
     system('clear')
   end
 end
