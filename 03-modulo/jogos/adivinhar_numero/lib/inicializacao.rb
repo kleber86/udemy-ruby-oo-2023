f542350f9f5f9f70a0618393773d75f5727dcd3f
Kleber Nascimento <klebernascimento@outlook.com.br>
2023-01-21 22:02:20 -0300

    ASCII Art

--- 03-modulo/jogos/adivinhar_numero/lib/inicializacao.rb
+++ 03-modulo/jogos/adivinhar_numero/lib/inicializacao.rb
@@ -1,12 +1,18 @@
 class Inicializacao
   def self.inicializando
     system('clear')
-    print "Inicializando."
-    4.times do |i|
-      sleep 1
-      print "."
+    # print "Inicializando."
+    # 4.times do |i|
+    #   sleep 1
+    #   print "."
+    # end
+    # puts "."
+    File.open(File.expand_path('../../ascii/ascii.txt', __FILE__), 'r') do |arq|
+      while line = arq.gets
+        puts line
+      end
     end
-    puts "."
+    sleep 6
     system('clear')
   end
 end
